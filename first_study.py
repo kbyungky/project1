75ff88200dbd8004ceb4048356ddfcc4c3f73b2d
kbyungky <kbyungky@dtcc.edu>
2015-10-22 10:00:52 -0400

    second

--- first_study.py
+++ first_study.py
@@ -2,10 +2,10 @@ __author__ = 'kbyungky'
 
 print("안녕하세요 미친넘아ㅋㅋㅋ")
 
-반복 = -1
-while 반복 != 0:
-    반복 = int(input("양의 정수 입력, 끝나면 0 : "))
-    print("배출한 값의 두배", 반복 * 2)
+qwer = -1
+while qwer != 0:
+    qwer = int(input("양의 정수 입력, 끝나면 0 : "))
+    print("배출한 값의 두배", qwer * 2)
 
 loop = -1
 while loop != 0:
@@ -14,5 +14,3 @@ while loop != 0:
         print(loop)
     else:
         print("It is over")
-
-print("졸림")
